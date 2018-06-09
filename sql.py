131fd65cd5c4ba736920ec6d829e575a381b861d
Development Laptop <ingenieria@borbolla-automation.com>
2018-06-09 18:36:32 -0500

    String format

--- sql.py
+++ sql.py
@@ -1,29 +1,15 @@
 #!usr/bin/env python3
-
-"""
-import pyodbc
-
-conn = pyodbc.connect("Driver ={SQL Server};Server=192.168.110.23;database=GMSDB;uid=gas;pw=gas")
-
-cur = conn.cursor("SELECT * FROM T1GaugeData")
-
-for row in cur:
-	for column in row:
-	    print(column)
-
-cur.close()
-conn.close()
-"""
-
-
+import datetime
 import pyodbc 
 # Some other example server values are
 # server = 'localhost\sqlexpress' # for a named instance
 # server = 'myserver,port' # to specify an alternate port
+
 con = pyodbc.connect('DRIVER=FreeTDS;SERVER=192.168.110.26;PORT=1433;DATABASE=GMSDB;UID=gas;PWD=gas;TDS_Version=7.0;')
 cursor = con.cursor()
 
-cursor.execute("SELECT GaugeID , GaugeName , BasicValue , Value , RangeUp , RangeDown , RegDate , RegTime FROM T1GaugeData WHERE GaugeID = 'P07'")
+cursor.execute("SELECT GaugeID , GaugeName , BasicValue , Value , RangeUp , RangeDown , RegDate , RegTime FROM T1GaugeData WHERE GaugeID = 'P07' AND RegDate = %s"%(str(datetime.datetime.now().year)+str("%02d"%(datetime.datetime.now().month,))+str("%02d"%(datetime.datetime.now().day),)
+) )
 
 row = cursor.fetchall()
 
