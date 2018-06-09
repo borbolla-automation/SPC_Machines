ed1d4a733601c069995e960f28385cfe4d0b150c
Development Laptop <ingenieria@borbolla-automation.com>
2018-06-09 18:50:15 -0500

    String format

--- sql.py
+++ sql.py
@@ -8,7 +8,7 @@ import pyodbc
 con = pyodbc.connect('DRIVER=FreeTDS;SERVER=192.168.110.26;PORT=1433;DATABASE=GMSDB;UID=gas;PWD=gas;TDS_Version=7.0;')
 cursor = con.cursor()
 
-cursor.execute("SELECT GaugeID , GaugeName , BasicValue , Value , RangeUp , RangeDown , RegDate , RegTime FROM T1GaugeData WHERE GaugeID = 'P07' AND RegDate = %s"%(str(datetime.datetime.now().year)+str("%02d"%(datetime.datetime.now().month,))+str("%02d"%(datetime.datetime.now().day),)))
+cursor.execute("SELECT SeqNum , GaugeID , GaugeName , BasicValue , Value , RangeUp , RangeDown , RegDate , RegTime FROM T1GaugeData WHERE GaugeID = 'P07' AND RegDate = %s"%(str(datetime.datetime.now().year)+str("%02d"%(datetime.datetime.now().month,))+str("%02d"%(datetime.datetime.now().day),)))
 
 row = cursor.fetchall()
 
