7d7f69a5879205e592ee4a7a9ec331aac73adc9c
Development Laptop <ingenieria@borbolla-automation.com>
2018-06-09 19:05:53 -0500

    String format

--- sql.py
+++ sql.py
@@ -7,10 +7,21 @@ import pyodbc
 
 con = pyodbc.connect('DRIVER=FreeTDS;SERVER=192.168.110.26;PORT=1433;DATABASE=GMSDB;UID=gas;PWD=gas;TDS_Version=7.0;')
 cursor = con.cursor()
+today_str = '%s'%(str(datetime.datetime.now().year)+str("%02d"%(datetime.datetime.now().month,))+str("%02d"%(datetime.datetime.now().day),))
 
-cursor.execute("SELECT SeqNum , GaugeID , GaugeName , BasicValue , Value , RangeUp , RangeDown , RegDate , RegTime FROM T1GaugeData WHERE GaugeID = 'P07' AND RegDate = %s"%(str(datetime.datetime.now().year)+str("%02d"%(datetime.datetime.now().month,))+str("%02d"%(datetime.datetime.now().day),)))
+cursor.execute("SELECT SeqNum , GaugeID , GaugeName , BasicValue , Value , RangeUp , RangeDown , RegDate , RegTime FROM T1GaugeData WHERE GaugeID = 'P07' AND RegDate = %s"%(today_str))
 
 row = cursor.fetchall()
+
+cursor.execute("SELECT TOP 1 SeqNum , RegDate  FROM T1GaugeData WHERE GaugeID = 'P07' AND regDate = %s ORDER BY SeqNum Desc"%(today_str))
+
+max_today = cursor.fetchone()
+
+cursor.execute("SELECT TOP 1 SeqNum , RegDate  FROM T1GaugeData WHERE GaugeID = 'P07' ORDER BY SeqNum Desc")
+
+max_total =  cursor.fetchone()
+
+print(max_total,max_total)
 
 for data in row:
 	print(data)
