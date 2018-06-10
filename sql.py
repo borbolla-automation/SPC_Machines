36c13bb80a0603733456520baefbd4645561862a
Development Laptop <ingenieria@borbolla-automation.com>
2018-06-09 20:09:57 -0500

    String format

--- sql.py
+++ sql.py
@@ -4,6 +4,22 @@ import pyodbc
 # Some other example server values are
 # server = 'localhost\sqlexpress' # for a named instance
 # server = 'myserver,port' # to specify an alternate port
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+scope = ['https://spreadsheets.google.com/feeds',
+		'https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('SPCMachines-47b188100dd5.json' , scope)
+client = gspread.authorize(creds)
+print(client)
+
+workbook = client.open_by_key('1o66bESi_ln3BL4RIHkROiljuELhL6HRdLfqIhCxXOsU')
+
+sheet = workbook.get_worksheet(9)
+
+
+
+
 
 con = pyodbc.connect('DRIVER=FreeTDS;SERVER=192.168.110.26;PORT=1433;DATABASE=GMSDB;UID=gas;PWD=gas;TDS_Version=7.0;')
 cursor = con.cursor()
@@ -26,6 +42,14 @@ max_total = int(max_total[0])
 print(today_str)
 print(max_today,max_total)
 
-"""for data in row:
-	print(data)"""
+i = 1
+j = 0
+for data in row:
+	for cell in data:
+		sheet.update_cell(i,j,cell)
+		j+=1
+	i+=1	
 
+records = sheet.get_all_records()
+
+print(records)
