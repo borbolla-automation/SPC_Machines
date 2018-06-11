818d315abbd0b57dafdb79ad0520d5fa73c8d50b
Development Laptop <ingenieria@borbolla-automation.com>
2018-06-11 11:30:09 -0500

    Query OOP

--- sql.py
+++ sql.py
@@ -1,4 +1,39 @@
 #!usr/bin/env python3
+
+#!usr/bin/env python3
+import datetime
+import pyodbc 
+# Some other example server values are
+# server = 'localhost\sqlexpress' # for a named instance
+# server = 'myserver,port' # to specify an alternate port
+#import gspread
+#from oauth2client.service_account import ServiceAccountCredentials
+
+class ODBCQuery(object):
+	"""docstring for ODBCQuery"""
+	def __init__(self, ip):
+		self.ip = ip
+		self.con = pyodbc.connect('DRIVER=FreeTDS;SERVER=192.168.110.26;PORT=1433;DATABASE=GMSDB;UID=gas;PWD=gas;TDS_Version=7.0;')
+		self.cursor = self.con.cursor()
+		self.today_str = '%s'%(str(datetime.datetime.now().year)+str("%02d"%(datetime.datetime.now().month,))+str("%02d"%(datetime.datetime.now().day),))
+
+	def today_registers(self , gauge):
+		self.cursor.execute("SELECT SeqNum , GaugeID , GaugeName , BasicValue , Value , RangeUp , RangeDown , RegDate , RegTime , result FROM T1GaugeData WHERE GaugeID = '%s' AND RegDate = %s"%(gauge , self.today_str))
+		self.data = self.cursor.fetchall()
+
+		return self.data
+
+if __name__ == '__main__':
+	odbc = ODBCQuery('192.168.110.26')
+	print(odbc.today_registers('P07'))
+
+
+
+
+
+
+
+"""
 import datetime
 import pyodbc 
 # Some other example server values are
@@ -58,4 +93,4 @@ for data in row:
 
 records = sheet.get_all_records()
 
-print(records)
+print(records)"""
