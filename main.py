39e519f1cbfe6af836a56ce5bf4b3886abff5321
Development Laptop <ingenieria@borbolla-automation.com>
2018-06-11 11:49:52 -0500

    xls test 1

--- main.py
+++ main.py
@@ -2,12 +2,15 @@
 
 from xls import Xlsx
 from sql import ODBCQuery
+from mail import SendMail
 
-#xls = Xlsx('SPC_test')
+xls = Xlsx('SPC_test')
 odbc = ODBCQuery('192.168.110.26')
-
+smail = SendMail(['administracion@borbolla-automation.com' , 'ingenieria@borbolla-automation.com' , 'bhleenk@kodaco.co.kr'] , 'SPC_test.xlsx' , subject = 'Today SPC 4G101 DP1 REGISTERS' )
 today_data  = odbc.today_registers('P07')
 print(today_data)
 
-#xls.write_data()
-#xls.close()
+xls.write_data()
+xls.close()
+
+smail.fast_send()
